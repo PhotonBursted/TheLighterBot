6f2609dea65f8527380b24c6dad31c5b612d6312
PhotonBursted <photonburstofficial@gmail.com>
2017-09-15 20:55:33 +0200

    Fleshed out launcher so it can load some standard Discord settings  - Also inserted Launcher#getBot to easily fetch the bot

--- src/main/java/st/photonbur/Discord/Bot/lightbotv3/main/Launcher.java
+++ src/main/java/st/photonbur/Discord/Bot/lightbotv3/main/Launcher.java
@@ -1,10 +1,21 @@
 package st.photonbur.Discord.Bot.lightbotv3.main;
 
+import net.dv8tion.jda.core.JDA;
+import st.photonbur.Discord.Bot.lightbotv3.controller.DiscordController;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * General manager of all parts of the bot.
  * Makes sure that everything is handled properly, from starting up to shutting down.
  */
 public class Launcher {
+    private Properties props = new Properties();
+
+    private DiscordController discordController;
+
     public static void main(String[] args) {
         new Launcher().run();
     }
@@ -13,7 +24,21 @@ public class Launcher {
      * Starts up the bot and every component necessary to work properly.
      */
     private void run() {
+        FileInputStream inputCfg = null;
+        try {
+            inputCfg = new FileInputStream("config.properties");
 
+            props.load(inputCfg);
+            discordController = new DiscordController(this, props.getProperty("token"), props.getProperty("prefix"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (inputCfg != null) try {
+                inputCfg.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     /**
@@ -22,5 +47,9 @@ public class Launcher {
      */
     private void shutdown() {
         Logger.shutdown();
+    }
+
+    public JDA getBot() {
+        return discordController.getBot();
     }
 }
