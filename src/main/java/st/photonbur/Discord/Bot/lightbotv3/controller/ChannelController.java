a79212de82cbe9692e0050185e1de3da23267c08
PhotonBursted <photonburstofficial@gmail.com>
2017-09-17 00:06:10 +0200

    Completed documentation on ChannelController

--- src/main/java/st/photonbur/Discord/Bot/lightbotv3/controller/ChannelController.java
+++ src/main/java/st/photonbur/Discord/Bot/lightbotv3/controller/ChannelController.java
@@ -24,9 +24,43 @@ import java.util.concurrent.TimeUnit;
 
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public class ChannelController extends ListenerAdapter {
+    private enum EventType {
+        LEAVE("left"), JOIN("joined"), MOVE("");
+
+        private final String verb;
+
+        EventType(String verb) {
+            this.verb = verb;
+        }
+
+        public String getVerb() {
+            return this.verb;
+        }
+    }
+
+    /**
+     * Stores the default category specified per guild
+     */
     private static final HashMap<Guild, Category> categories;
+    /**
+     * Stores a timeout per voice channel.
+     * Makes sure that, when a voice channel is created, it gets deleted when it is left unused.
+     */
     private static final HashMap<VoiceChannel, ScheduledExecutorService> timeoutCandidates;
-    private static final HashMap<VoiceChannel, TextChannel> linkedChannels, permChannels;
+    /**
+     * Keeps track of channels which are linked to each other.
+     * This "linkage" is referring to the displaying of join, leave and move actions in the text channel.
+     */
+    private static final HashMap<VoiceChannel, TextChannel> linkedChannels;
+    /**
+     * Keeps track of pairs of channels which should not be removed when empty.
+     */
+    private static final HashMap<VoiceChannel, TextChannel> permChannels;
+
+    /**
+     * Instance of the launcher for easy access to other classes
+     */
+    private final Launcher l;
 
     static {
         categories = new HashMap<>();
@@ -35,38 +69,47 @@ public class ChannelController extends ListenerAdapter {
         timeoutCandidates = new HashMap<>();
     }
 
-    private final Launcher l;
-
     public ChannelController(Launcher l) {
         this.l = l;
     }
 
-    public static HashMap<Guild, Category> getCategories() {
-        return categories;
-    }
-
-    public static HashMap<VoiceChannel, TextChannel> getLinkedChannels() {
-        return linkedChannels;
-    }
-
-    public static HashMap<VoiceChannel, TextChannel> getPermChannels() {
-        return permChannels;
-    }
-
-    public static Category createTempCategory(Guild g, String channelName) {
-        ChannelAction cAction = g.getController().createCategory("Temp: " + channelName);
-
+    /**
+     * Applies the blacklist to a certain channel right before it is constructed.
+     *
+     * @param g      The guild the channel is in
+     * @param action The action representing the channel to apply the blacklist to
+     * @param perms  The permissions to apply
+     */
+    private static void applyBlacklist(Guild g, ChannelAction action, Permission... perms) {
+        // Figures out the blacklist present in this guild
         Set<? extends ISnowflake> blacklist = BlacklistController.getForGuild(g);
         if (blacklist != null) {
+            // If applicable, deny people their access of this category
             blacklist.forEach(item -> {
                 if (item instanceof Role) {
-                    cAction.addPermissionOverride((Role) item, 0L, Permission.getRaw(Permission.MESSAGE_READ));
+                    action.addPermissionOverride((Role) item, 0L, Permission.getRaw(perms));
                 } else if (item instanceof User) {
-                    cAction.addPermissionOverride(g.getMember((User) item), 0L, Permission.getRaw(Permission.MESSAGE_READ));
+                    action.addPermissionOverride(g.getMember((User) item), 0L, Permission.getRaw(perms));
                 }
             });
         }
+    }
 
+    /**
+     * Creates a temporary category to house a fresh set of temporary channels in.
+     *
+     * @param g    The guild to put the new category in.
+     * @param name The name to give the category.
+     * @return The category created by this method.
+     */
+    public static Category createTempCategory(Guild g, String name) {
+        // Creates an intermediate action which only contains the name of the new category.
+        ChannelAction cAction = g.getController().createCategory("Temp: " + name);
+
+        applyBlacklist(g, cAction, Permission.MESSAGE_READ);
+
+        // Finally construct the category object.
+        // This construction is split so it only requires one request instead of multiple.
         Category c = (Category) cAction
                 .setParent(categories.get(g))
                 .reason("A new set of temporary channels was issued, needing an new category as no default was specified")
@@ -80,7 +123,16 @@ public class ChannelController extends ListenerAdapter {
         return c;
     }
 
+    /**
+     * Creates a temporary text channel.
+     *
+     * @param ev     The event which caused the new channel to be created.
+     * @param name   The name to give the channel.
+     * @param parent The category to house the new channel in.
+     * @return The category created by this method.
+     */
     public static TextChannel createTempTextChannel(GuildMessageReceivedEvent ev, String name, Category parent) throws RateLimitedException {
+        // Creates an intermediate action which only contains the name of the new text channel.
         ChannelAction tcAction = ev.getGuild().getController().createTextChannel(Utils.ircify("tdc-" + name))
                 .addPermissionOverride(ev.getGuild().getPublicRole(),
                         Permission.getRaw(Permission.MESSAGE_READ, Permission.MESSAGE_WRITE, Permission.MESSAGE_HISTORY),
@@ -90,17 +142,10 @@ public class ChannelController extends ListenerAdapter {
                         0
                 );
 
-        // SHOULD BE REMOVED ONCE CATEGORIES ARE PUBLIC!!!
-        Set<? extends ISnowflake> blacklist = BlacklistController.getForGuild(ev.getGuild());
-        if (blacklist != null) {
-            blacklist.forEach(item -> {
-                if (item instanceof Role) {
-                    tcAction.addPermissionOverride((Role) item, 0L, Permission.getRaw(Permission.MESSAGE_READ));
-                } else if (item instanceof User) {
-                    tcAction.addPermissionOverride(ev.getGuild().getMember((User) item), 0L, Permission.getRaw(Permission.MESSAGE_READ));
-                }
-            });
-        }
+        applyBlacklist(ev.getGuild(), tcAction, Permission.MESSAGE_READ);
+
+        // Finally construct the category object.
+        // This construction is split so it only requires one request instead of multiple.
         TextChannel tc = (TextChannel) tcAction
                 .setParent(parent)
                 .reason("A new temporary channel was issued by " + Utils.userAsString(ev.getAuthor()))
@@ -114,25 +159,25 @@ public class ChannelController extends ListenerAdapter {
         return tc;
     }
 
-    // Create a temporary voice channel and notify the user of it.
+    /**
+     * Creates a temporary voice channel.
+     *
+     * @param ev     The event which caused the new channel to be created.
+     * @param name   The name to give the channel.
+     * @param parent The category to house the new channel in.
+     * @return The category created by this method.
+     */
     public static VoiceChannel createTempVoiceChannel(GuildMessageReceivedEvent ev, String name, Category parent) throws RateLimitedException {
+        // Creates an intermediate action which only contains the name of the new voice channel.
         ChannelAction vcAction = ev.getGuild().getController().createVoiceChannel("[T] " + name)
                 .addPermissionOverride(ev.getGuild().getPublicRole(),
                         Permission.getRaw(Permission.MESSAGE_READ, Permission.VOICE_CONNECT),
                         0);
 
-        // SHOULD BE REMOVED ONCE CATEGORIES ARE PUBLIC!!!
-        Set<? extends ISnowflake> blacklist = BlacklistController.getForGuild(ev.getGuild());
-        if (blacklist != null) {
-            blacklist.forEach(item -> {
-                if (item instanceof Role) {
-                    vcAction.addPermissionOverride((Role) item, 0L, Permission.getRaw(Permission.VOICE_CONNECT, Permission.MESSAGE_READ));
-                } else if (item instanceof User) {
-                    vcAction.addPermissionOverride(ev.getGuild().getMember((User) item), 0L, Permission.getRaw(Permission.VOICE_CONNECT, Permission.MESSAGE_READ));
-                }
-            });
-        }
+        applyBlacklist(ev.getGuild(), vcAction, Permission.MESSAGE_READ, Permission.VOICE_CONNECT);
 
+        // Finally construct the category object.
+        // This construction is split so it only requires one request instead of multiple.
         VoiceChannel vc = (VoiceChannel) vcAction
                 .setParent(parent)
                 .reason("A new temporary channel was issued by " + Utils.userAsString(ev.getAuthor()))
@@ -146,6 +191,13 @@ public class ChannelController extends ListenerAdapter {
         return vc;
     }
 
+    /**
+     * Deletes a channel that was once linked.
+     * If it was and not made permanent it was temporary and can therefore be deleted when obsolete.
+     *
+     * @param c The channel to remove
+     * @return The success of the operation
+     */
     private static boolean deleteLinkedChannel(Channel c) {
         if (c != null) {
             c.delete().reason("This temporary channel had no people left in it").queue();
@@ -162,15 +214,24 @@ public class ChannelController extends ListenerAdapter {
         }
     }
 
+    /**
+     * Deletes a set of linked channels and, if necessary, its parent as well.
+     *
+     * @param vc  The voice channel to remove. The linked channels are stored with {@link VoiceChannel} as key.
+     * @param ses The timeout executor to shut down
+     */
     private static void deleteLinkedChannels(VoiceChannel vc, @Nullable ScheduledExecutorService ses) {
+        // If any timeout executor is present, shut it down
         if (ses != null) {
             ses.shutdown();
         }
 
+        // Try to remove all channels involved
         boolean cleaned = deleteLinkedChannel(vc) &&
                 deleteLinkedChannel(linkedChannels.get(vc)) &&
                 ((categories.containsKey(vc.getGuild()) && categories.get(vc.getGuild()) != null) || deleteLinkedChannel(vc.getParent()));
 
+        // Log feedback
         if (cleaned) {
             Logger.log("Successfully cleaned up group " + vc.getName() + "!");
         } else {
@@ -178,22 +239,26 @@ public class ChannelController extends ListenerAdapter {
         }
     }
 
-    public static void setNewChannelTimeout(VoiceChannel vc) {
-        ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
-        timeoutCandidates.put(vc, ses);
-
-        ses.scheduleWithFixedDelay(() -> deleteLinkedChannels(vc, ses), 10, 10, TimeUnit.SECONDS);
-    }
-
+    /**
+     * Deletes a set of linked channels and, if necessary, its parent as well.
+     *
+     * @param vc  The voice channel to remove. The linked channels are stored with {@link VoiceChannel} as key.
+     */
     private void deleteLinkedChannels(VoiceChannel vc) {
         deleteLinkedChannels(vc, null);
     }
 
-    private Map<VoiceChannel, TextChannel> fetchChannels() {
-        Logger.log("Fetching permanent channel pairs...");
+    /**
+     * Fetches all channel pairs which should be linked together.
+     * This both validates the channel pairs stored in the channel file and gathers pairs of still-active temporary channels.
+     * @return The pairing of all linked channels
+     */
+    private Map<VoiceChannel, TextChannel> fetchChannelPairs() {
+        Logger.log("Fetching channel pairs...");
 
         Map<VoiceChannel, TextChannel> ids = new HashMap<>();
 
+        // Validate stored channels
         permChannels.forEach((vc, tc) -> {
             if (vc != null && tc != null) {
                 if (tc.canTalk() && vc.getGuild().equals(tc.getGuild())) {
@@ -202,6 +267,7 @@ public class ChannelController extends ListenerAdapter {
             }
         });
 
+        // Find already active temporary channels
         l.getBot().getVoiceChannels().stream().filter(vc -> vc.getName().startsWith("[T] ")).forEach(vc -> {
             List<TextChannel> tcl = vc.getGuild().getTextChannelsByName(Utils.ircify("tdc-" + vc.getName().substring(4)), true);
 
@@ -210,11 +276,41 @@ public class ChannelController extends ListenerAdapter {
             }
         });
 
-        Logger.log("Found permanent channel pairs: " + ids.size() + "\n" + Utils.getFetchedChannelMapAsString(ids));
+        // Log the found pairs
+        Logger.log("Found channel pairs: " + ids.size() + "\n" + Utils.getFetchedChannelMapAsString(ids));
 
         return ids;
     }
 
+    /**
+     * @return The categories saved for every guild.
+     */
+    public static HashMap<Guild, Category> getCategories() {
+        return categories;
+    }
+
+    /**
+     * @return The pairs of channels linked to each other.
+     */
+    public static HashMap<VoiceChannel, TextChannel> getLinkedChannels() {
+        return linkedChannels;
+    }
+
+    /**
+     * @return The pairs of channels which should be kept, even when left empty.
+     */
+    public static HashMap<VoiceChannel, TextChannel> getPermChannels() {
+        return permChannels;
+    }
+
+    /**
+     * Informs the users about an event that happened.
+     * This usually relates to voice channel activity.
+     *
+     * @param type   The type of event to inform about
+     * @param member The member who triggered the event
+     * @param vcs    The voice channel(s) in question
+     */
     private void informUserAbout(EventType type, Member member, VoiceChannel... vcs) {
         switch (type) {
             case JOIN:
@@ -237,6 +333,13 @@ public class ChannelController extends ListenerAdapter {
         }
     }
 
+    /**
+     * Logs an event to the logs.
+     *
+     * @param type   The type of event to log
+     * @param member The member who triggered the event
+     * @param vcs    The voice channel(s) in question
+     */
     private void logEvent(@Nullable EventType type, Member member, VoiceChannel... vcs) {
         String message = "";
 
@@ -261,6 +364,11 @@ public class ChannelController extends ListenerAdapter {
         Logger.log(message);
     }
 
+    /**
+     * Fired when a member joins a voice channel.
+     *
+     * @param ev The event thrown after the joining has occurred
+     */
     @Override
     public void onGuildVoiceJoin(GuildVoiceJoinEvent ev) {
         logEvent(EventType.JOIN, ev.getMember(), ev.getChannelJoined());
@@ -269,6 +377,11 @@ public class ChannelController extends ListenerAdapter {
         informUserAbout(EventType.JOIN, ev.getMember(), ev.getChannelJoined());
     }
 
+    /**
+     * Fired when a member moves between voice channels.
+     *
+     * @param ev The event thrown after the moving has occurred
+     */
     @Override
     public void onGuildVoiceMove(GuildVoiceMoveEvent ev) {
         logEvent(null, ev.getMember(), ev.getChannelLeft(), ev.getChannelJoined());
@@ -277,6 +390,11 @@ public class ChannelController extends ListenerAdapter {
         informUserAbout(EventType.MOVE, ev.getMember(), ev.getChannelJoined(), ev.getChannelLeft());
     }
 
+    /**
+     * Fired when a member leaves a voice channel.
+     *
+     * @param ev The event thrown after the leaving has occurred
+     */
     @Override
     public void onGuildVoiceLeave(GuildVoiceLeaveEvent ev) {
         logEvent(EventType.LEAVE, ev.getMember(), ev.getChannelLeft());
@@ -285,11 +403,21 @@ public class ChannelController extends ListenerAdapter {
         informUserAbout(EventType.LEAVE, ev.getMember(), ev.getChannelLeft());
     }
 
+    /**
+     * Fired when a member joins a voice channel.
+     *
+     * @param vc The voice channel which was joined
+     * @param m  The member joining a voice channel
+     */
     private void respondToJoin(VoiceChannel vc, Member m) {
+        // If a shutdown was still scheduled, cancel it.
+        // If a user joined the channel, it should be kept alive.
         if (timeoutCandidates.containsKey(vc)) {
             timeoutCandidates.get(vc).shutdown();
         }
 
+        // If this is a linked channel, the member should have reading permissions within the text channel
+        // should the voice channel be limited.
         if (linkedChannels.containsKey(vc)) {
             if (Utils.hasLimit(vc)) {
                 try {
@@ -301,16 +429,35 @@ public class ChannelController extends ListenerAdapter {
         }
     }
 
+
+    /**
+     * Fired when a member joins a voice channel.
+     *
+     * @param vcj The voice channel which was joined
+     * @param vcl The voice channel which was left
+     * @param m  The member joining a voice channel
+     */
     private void respondToMove(VoiceChannel vcj, VoiceChannel vcl, Member m) {
+        // Moving corresponds to leaving and joining at the same time, hence do both actions
         respondToJoin(vcj, m);
         respondToLeave(vcl, m);
     }
 
+    /**
+     * Fired when a member leave a voice channel.
+     *
+     * @param vc The voice channel which was left
+     * @param m  The member leaving a voice channel
+     */
     private void respondToLeave(VoiceChannel vc, Member m) {
+        // If a shutdown was still scheduled, cancel it.
+        // If a user joined the channel, it should be kept alive.
         if (vc.getMembers().size() == 0 && linkedChannels.containsKey(vc) && !permChannels.containsKey(vc)) {
             deleteLinkedChannels(vc);
         }
 
+        // If this is a linked channel, the member should not have reading permissions within the text channel
+        // should the voice channel be limited.
         if (linkedChannels.containsKey(vc)) {
             if (Utils.hasLimit(vc)) {
                 try {
@@ -322,17 +469,16 @@ public class ChannelController extends ListenerAdapter {
         }
     }
 
-    private enum EventType {
-        LEAVE("left"), JOIN("joined"), MOVE("");
+    /**
+     * Creates a new timeout for the specified channel.
+     * When the timeout runs out, the channel will be deleted along with the dedicated channels.
+     *
+     * @param vc The voice channel to create the timeout with
+     */
+    public static void setNewChannelTimeout(VoiceChannel vc) {
+        ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+        timeoutCandidates.put(vc, ses);
 
-        private final String verb;
-
-        EventType(String verb) {
-            this.verb = verb;
-        }
-
-        public String getVerb() {
-            return this.verb;
-        }
+        ses.scheduleWithFixedDelay(() -> deleteLinkedChannels(vc, ses), 10, 10, TimeUnit.SECONDS);
     }
 }
