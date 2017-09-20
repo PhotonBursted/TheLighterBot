536500459bf493b5624b47d56ed087f090cbcaff
PhotonBursted <photonburstofficial@gmail.com>
2017-09-21 01:10:39 +0200

    Slight tweaks in logger to avoid some clutter

--- src/main/java/st/photonbur/Discord/Bot/lightbotv3/main/Logger.java
+++ src/main/java/st/photonbur/Discord/Bot/lightbotv3/main/Logger.java
@@ -4,7 +4,6 @@ import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.events.ExceptionEvent;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
-import net.dv8tion.jda.core.requests.RestAction;
 import net.dv8tion.jda.core.utils.SimpleLog;
 import st.photonbur.Discord.Bot.lightbotv3.command.CommandParser;
 
@@ -99,8 +98,7 @@ public class Logger extends ListenerAdapter {
             // Check if the stack trace element is currently from within the Logger class or the last available
             if ((
                     !ste.getClassName().contains(Logger.class.getName()) &&
-                    !ste.getClassName().contains(SimpleLog.class.getName()) &&
-                    !ste.getClassName().contains(RestAction.class.getName())
+                    !ste.getClassName().contains(SimpleLog.class.getName())
             ) || i == stes.length - 1) {
                 // Determine the actual stack trace element target (compensated by the offset)
                 ste = stes[Arrays.asList(stes).indexOf(ste) - offset];
@@ -182,8 +180,10 @@ public class Logger extends ListenerAdapter {
 
                 @Override
                 public void onLog(SimpleLog log, SimpleLog.Level logLevel, Object message) {
-                    if (logLevel.getPriority() > 2 && !message.toString().contains("Encountered an exception")) {
-                        log(String.format("[%s] [%s]: %s", log.name, logLevel.getTag().toUpperCase(), message));
+                    if (logLevel.getPriority() > 2) {
+                        if (!log.name.equals("RestAction") && !message.toString().contains("10008")) {
+                            log(String.format("[%s] [%s]: %s", log.name, logLevel.getTag().toUpperCase(), message), -2);
+                        }
                     }
                 }
             });
